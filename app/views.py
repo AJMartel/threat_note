5a7563d7483e4b4251e227d1d708258747ad9f5f
Alex Herrick <alex.herrick@cdk.coom>
2016-09-21 15:36:14 -0500

    Moar Changes

--- app/views.py
+++ app/views.py
@@ -64,3 +64,177 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+
+@app.route('/', methods=['GET'])
+@login_required
+def home():
+    try:
+        counts = Indicator.query.distinct(Indicator.indicator).count()
+        types = Indicator.query.group_by(Indicator.indicator_type).all()
+        #network = Indicator.query.order_by(Indicator.indicator.desc()).limit(5).all()
+        campaigns = Campaign.query.group_by(Campaign.name).all()
+        taglist = Indicator.query.distinct(Indicator.tags).all()
+
+        network = Indicator.query.join(Campaign, Indicator.campaign_id == Campaign._id).order_by(Indicator._id.desc()).limit(5).all()
+        for i in network:
+            print i.name
+        # Generate Tag Cloud
+        tags = set()
+        for indicator in taglist:
+            if indicator.tags == "":
+                pass
+            else:
+                for tag in indicator.tags.split(","):
+                    tags.add(tag.strip())
+
+        dictcount = {}
+        dictlist = []
+        typecount = {}
+        typelist = []
+
+        # Generate Campaign Statistics Graph
+        for campaign in campaigns:
+            c = Indicator.query.filter_by(campaign_id=campaign.get_id()).count()
+            if campaign.name == '':
+                dictcount["category"] = "Unknown"
+                tempx = (float(c) / float(counts)) * 100
+                dictcount["value"] = round(tempx, 2)
+            else:
+                dictcount["category"] = campaign.name
+                tempx = (float(c) / float(counts)) * 100
+                dictcount["value"] = round(tempx, 2)
+
+            dictlist.append(dictcount.copy())
+
+        # Generate Indicator Type Graph
+        for t in types:
+            c = Indicator.query.filter_by(indicator_type=t.indicator_type).count()
+            typecount["category"] = t.indicator_type
+            tempx = float(c) / float(counts)
+            newtemp = tempx * 100
+            typecount["value"] = round(newtemp, 2)
+            typelist.append(typecount.copy())
+        favs = []
+
+        # Add Import from Cuckoo button to Dashboard page
+        settings = Setting.query.filter_by(_id=1).first()
+        if 'on' in settings.cuckoo:
+            importsetting = True
+        else:
+            importsetting = False
+
+        return render_template('dashboard.html', networks=dictlist, network=network, favs=favs, typelist=typelist,
+                               taglist=tags, importsetting=importsetting)
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+
+@app.route('/networks', methods=['GET'])
+@login_required
+def networks():
+    try:
+        # Grab only network indicators
+        network = Indicator.query.filter(Indicator.indicator_type.in_(('IPv4', 'IPv6', 'Domain', 'Network'))).all()
+        return render_template('indicatorlist.html', network=network, title='Network Indicators', links='network')
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+
+@app.route('/threatactors', methods=['GET'])
+@login_required
+def threatactors():
+    try:
+        # Grab threat actors
+        threatactors = Indicator.query.filter(Indicator.indicator_type == 'Threat Actor').all()
+        return render_template('indicatorlist.html', network=threatactors, title='Threat Actors', links='threatactors')
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+
+@app.route('/victims', methods=['GET'])
+@login_required
+def victims():
+    try:
+        # Grab victims
+        victims = Indicator.query.filter(Indicator.diamondmodel == ('Victim')).all()
+        return render_template('indicatorlist.html', network=victims, title='Victims', links='victims')
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+
+@app.route('/files', methods=['GET'])
+@login_required
+def files():
+    try:
+        # Grab files/hashes
+        files = Indicator.query.filter(Indicator.indicator_type == ('Hash')).all()
+        return render_template('indicatorlist.html', network=files, title='Files & Hashes', links='files')
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+
+@app.route('/campaigns', methods=['GET'])
+@login_required
+def campaigns():
+    try:
+        # Grab campaigns
+        campaignents = dict()
+        rows = Campaign.query.group_by(Campaign.name).all()
+        for c in rows:
+            if c.campaign == '':
+                name = 'Unknown'
+            else:
+                name = c.campaign
+            campaignents[name] = list()
+        # Match indicators to campaigns
+        for camp, indicators in campaignents.iteritems():
+            if camp == 'Unknown':
+                camp = ''
+            rows = Indicator.query.filter(Indicator.campaign == camp).all()
+            tmp = {}
+            for i in rows:
+                tmp[i.object] = i.indicator_type
+                indicators.append(tmp)
+        return render_template('campaigns.html', campaignents=campaignents)
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+
+
+@app.route('/tags', methods=['GET'])
+@login_required
+def tags():
+    try:
+        # Grab tags
+        taglist = dict()
+        rows = Indicator.query.distinct(Indicator.tags).all()
+        if rows:
+            for row in rows:
+                if row.tags:
+                    for tag in row.tags.split(','):
+                        taglist[tag.strip()] = list()
+            # Match indicators to tags
+            del rows, row
+            for tag, indicators in taglist.iteritems():
+                rows = Indicator.query.filter(Indicator.tags.like('%' + tag + '%')).all()
+                tmp = {}
+                for row in rows:
+                    tmp[row.object] = row.indicator_type
+                    indicators.append(tmp)
+
+        return render_template('tags.html', tags=taglist)
+    except Exception as e:
+        return render_template('error.html', error=e)
+
+
+@app.route('/settings', methods=['GET'])
+@login_required
+def settings():
+    try:
+        settings = Setting.query.filter_by(_id=1).first()
+        user = User.query.filter(User.user == current_user).first
+        return render_template('settings.html', records=settings, suser=user)
+    except Exception as e:
+        return render_template('error.html', error=e)
+
---
+++ run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+#
+# threat_note v3.0                                      #
+# Developed By: Brian Warehime                          #
+# Defense Point Security (defpoint.com)                 #
+# October 26, 2015                                      #
+#
+
+import argparse
+from app import app
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H', '--host', default="127.0.0.1", help="Specify the host IP address")
+    parser.add_argument('-p', '--port', default=8888, help="Specify port to listen on")
+    parser.add_argument('-d', '--debug', default=False, help="Run in debug mode", action="store_true")
+    parser.add_argument('-db', '--database', help="Path to sqlite database - Not Implemented")
+    args = parser.parse_args()
+
+    app.run(host=args.host, port=args.port, debug=args.debug)
