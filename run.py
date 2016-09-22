992c764e0c829a435f7a6c2af9896e6292951150
Alex Herrick <alex hrck net>
2016-09-21 21:55:06 -0500

    adding ablility to specify database file

--- run.py
+++ run.py
@@ -17,10 +17,14 @@ if __name__ == '__main__':
     parser.add_argument('-H', '--host', default="127.0.0.1", help="Specify the host IP address")
     parser.add_argument('-p', '--port', default=8888, help="Specify port to listen on")
     parser.add_argument('-d', '--debug', default=False, help="Run in debug mode", action="store_true")
-    parser.add_argument('-db', '--database', help="Path to sqlite database - Not Implemented")
+    parser.add_argument('-b', '--database', default="threatnote.db", help="Path to sqlite database - Not Implemented")
     args = parser.parse_args()
 
-    if not os.path.isfile(os.path.join(os.getcwd(), 'tmp', 'test.db')):
+    database = 'test.db'
+
+    if args.database:
+        database = args.database
+    if not os.path.isfile(os.path.join(os.getcwd(), 'tmp', database)):
         from app import db
         db.create_all()
 
